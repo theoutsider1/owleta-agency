04ffdbdc245b339e1cc8b1c6ffdc4a8e42788461
Hatim Tagmi <hatim.tagmi@ef2conseil.com>
2025-11-21 23:55:09 +0100

    feat: include ServicesSection component in main.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/hero-section";
+import { ServicesSection } from "@/components/services-section";
 
 export default function Home() {
   return (
       <main className="min-h-screen">
         <HeroSection />
+        <ServicesSection/>
       </main>
   );
 }
