5a39989fd5eba1bd97acca47837607004f84a24a
Hatim Tagmi <hatim.tagmi@ef2conseil.com>
2025-11-23 00:55:14 +0100

    feat: add navigation and footer to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { ContactSection } from "@/components/contact-section";
+import { Footer } from "@/components/footer";
 import { HeroSection } from "@/components/hero-section";
+import { Navigation } from "@/components/navigation";
 import { ProcessSection } from "@/components/process-section";
 import { ServicesSection } from "@/components/services-section";
 import { WhyChooseUsSection } from "@/components/why-choose-us-section";
@@ -7,11 +9,13 @@ import { WhyChooseUsSection } from "@/components/why-choose-us-section";
 export default function Home() {
   return (
       <main className="min-h-screen">
+        <Navigation/>
         <HeroSection />
         <ServicesSection/>
         <ProcessSection/>
         <WhyChooseUsSection/>
         <ContactSection/>
+        <Footer/>
       </main>
   );
 }
